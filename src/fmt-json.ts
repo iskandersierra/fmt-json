50279b0dbbb24c7194f89d9297a040c24202ef3f
isierra <isierra@jgingenieros.es>
2018-02-07 18:41:27 +0100

    WIP

--- src/fmt-json.ts
+++ src/fmt-json.ts
@@ -1,4 +1,18 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
+// https://www.npmjs.com/package/format-json
+
+/**
+ * mode: diffy, plain, terse (default), space, lines
+ * printConstructor: true | false (default) |
+ * indentChar: one char or space
+ * indentCount: chars per indent
+ * avoidQuotes: boolean
+ * maxStringLength: number
+ * maxTextLength: number
+ * maxArrayLength: number
+ * maxObjectLength: number
+ * cappedStringTail: number
+ * cappedArrayTail: number
+ * skipObject: Function
+ * skipTypes: array of (typeof)
+ */
 export default class DummyClass {}
